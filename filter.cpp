098ddd8bc23f5df560d9060b3bbd76555c3a948b
Ramesh RAJABY <rameshr@n126.default.domain>
2018-06-25 18:09:11 +0800

    Make results more readable

--- filter.cpp
+++ filter.cpp
@@ -19,43 +19,22 @@ double ptn_score(const prediction_t& pred) {
     return double(pred.disc_pairs+pred.bp1.sc_reads)/std::max(pred.bp1.spanning_reads,1);
 }
 
+char dir_to_strand(char dir) {
+    return (dir == 'R' ? 'F' : 'R');
+}
+
 std::string formatted_print(prediction_t& pred) {
     char str[10000];
-    if (pred.bp1.sc_reads > 0 && pred.sv_type != SV_TYPES.INS) {
-        sprintf(str, "ID=%d BP1=%c:%s:%d BP2=%c:%s:%d %s DISC=%d SC=%d,%d SPANNING=%d,%d PVAL=%lf EST_SIZE=%d SHIFT-PVAL=%lf",
-                pred.id,
-                pred.bp1.dir, contig_id2name[pred.bp1.contig_id].c_str(), pred.bp1.pos(),
-                pred.bp2.dir, contig_id2name[pred.bp2.contig_id].c_str(), pred.bp2.pos(),
-                svt_to_str(pred.sv_type).c_str(), pred.disc_pairs, pred.bp1.sc_reads, pred.bp2.sc_reads,
-                pred.bp1.spanning_reads, pred.bp2.spanning_reads, pred.pval, pred.get_size(), pred.shift_pval);
-    } else {
-        sprintf(str, "ID=%d BP1=%c:%s:%d BP2=%c:%s:%d %s DISC=%d SC=%d,%d SPANNING=%d,%d PVAL=%lf EST_SIZE=%d:%d SHIFT-PVAL=%lf",
-                pred.id,
-                pred.bp1.dir, contig_id2name[pred.bp1.contig_id].c_str(), pred.bp1.pos(),
-                pred.bp2.dir, contig_id2name[pred.bp2.contig_id].c_str(), pred.bp2.pos(),
-                svt_to_str(pred.sv_type).c_str(), pred.disc_pairs, pred.bp1.sc_reads, pred.bp2.sc_reads,
-                pred.bp1.spanning_reads, pred.bp2.spanning_reads, pred.pval, pred.size - pred.conf_ival,
-                pred.size + pred.conf_ival, pred.shift_pval);
-    }
+    sprintf(str, "ID=%d BP1=%c:%s:%d BP2=%c:%s:%d DISCORDANT=%d SCORE=%lf",
+            pred.id,
+            dir_to_strand(pred.bp1.dir), contig_id2name[pred.bp1.contig_id].c_str(), pred.bp1.pos(),
+            dir_to_strand(pred.bp2.dir), contig_id2name[pred.bp2.contig_id].c_str(), pred.bp2.pos(),
+            pred.disc_pairs, ptn_score(pred));
     return str;
 }
 
-std::unordered_set<int> in_reps_ids;
-
 bool operator < (const prediction_t& p1, const prediction_t& p2) {
-//    bool in_rep1 = in_reps_ids.count(p1.id);
-//    bool in_rep2 = in_reps_ids.count(p2.id);
-//    if (in_rep1 != in_rep2) return in_rep1 < in_rep2;
-//    if (p1.bp1.sc_reads*p1.bp2.sc_reads != p2.bp1.sc_reads*p2.bp2.sc_reads) {
-//        return p1.bp1.sc_reads*p1.bp2.sc_reads > p2.bp1.sc_reads*p2.bp2.sc_reads;
-//    }
-//    if (p1.disc_pairs != p2.disc_pairs) {
-//        return p1.disc_pairs > p2.disc_pairs;
-//    }
-//    if (p1.pval != p2.pval) {
-//        return p1.pval < p2.pval;
-//    }
-    return ptn_score(p1) > ptn_score(p2);
+//    return ptn_score(p1) > ptn_score(p2);
     return p1.id < p2.id;
 }
 
@@ -97,6 +76,6 @@ int main(int argc, char* argv[]) {
 
     std::sort(retained.begin(), retained.end());
     for (prediction_t pred : retained) {
-        std::cout << formatted_print(pred) << " " << in_reps_ids.count(pred.id) << std::endl;
+        std::cout << formatted_print(pred) << std::endl;
     }
 }
